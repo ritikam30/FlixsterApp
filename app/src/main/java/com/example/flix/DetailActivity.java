0f5d760560e26c54b080a9b589a25db1400573b4
ritikam30 <56143777+ritikam30@users.noreply.github.com>
2020-10-10 14:06:11 -0500

    Update DetailActivity.java

--- app/src/main/java/com/example/flix/DetailActivity.java
+++ app/src/main/java/com/example/flix/DetailActivity.java
@@ -22,7 +22,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyDcznHV3Wx77-BYVnoiCe_uiBV1JaqGvhk";
+    public static final String YOUTUBE_API_KEY = "AIzaSyAHpKACyrPHDXX1CXmkAhjnJLapTc_Z888";
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     TextView tvTitle;
     TextView tvOverview;
